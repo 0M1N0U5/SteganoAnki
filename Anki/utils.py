6bab96d4a5332f194a01712abbab034a6a37885b
Jose Maria <josemafc-1@hotmail.com>
2021-05-11 02:00:36 +0200

    Sha256 para ficheros para futuras colisiones

--- Anki/utils.py
+++ Anki/utils.py
@@ -119,6 +119,14 @@ def sha256Iterations(password, iterations):
         seed = sha256(password + seed)
     return seed
 
+def calculateSha256File(filename):
+    sha256_hash = hashlib.sha256()
+    with open(filename,"rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
 def ofuscate(data):
     #print("Ofuscate:", data)
     return data

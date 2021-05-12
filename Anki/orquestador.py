7e5889c8391743f84248d8f29826e22809ae502e
Jose Maria <josemafc-1@hotmail.com>
2021-05-12 16:09:51 +0200

    Limpieza orquestador

--- Anki/orquestador.py
+++ Anki/orquestador.py
@@ -19,6 +19,14 @@ def codificar(rutaBase, index, nombreImagen, mensaje, password):
     else:
         return False
 
+def buscarImagenesMazo(rutaBase, mazo, estimate=False):
+    lista = []
+    for index, row in mazo.iterrows():
+        resultadoAnalisis = analizarCard(rutaBase, row.name, row.flds, estimate)
+        if len(resultadoAnalisis) != 0:
+            lista.append(resultadoAnalisis)
+    return lista
+
 def modificarNombre(nombreImagen):
     index = nombreImagen.rfind(".")
     return nombreImagen[:index] + "_.png"
@@ -33,59 +41,6 @@ def analizarCard(rutaBase, index, campoFlds, estimacionReal=False): #Por ahora s
         image = {"name": i, "estimacion": estimacion, "index": index}
         respuesta.append(image)
     return respuesta
-
-def main():
-    supossedMain()
-    exit(0)
-    mocking = True
-    mocking = False
-    rutaBase = "/home/jose/.local/share/Anki2/Usuario 1/collection.media/"
-    resultado = [[{"name": "gonzalo-madrid.jpg", "estimacion" : 91693, "index": 477}], [{"name": "Jose-madrid.jpg", "estimacion": 37653, "index": 478}]]
-    data = utils.stringToHex("Esto")
-    password = "password123"
-    if not mocking:
-        aw = AnkiWrapper.getInstance()
-        rutaBase = aw.rutaBase
-        nombreMazo = "Gonzalo"
-        mensaje = utils.stringToHex("Hola, este es un mensaje:)")
-        mazo = aw.getNotesFromDeck(nombreMazo)
-        print(mazo)
-        print("---")
-        print("Calcular")
-        resultado = buscarImagenesMazo(rutaBase, mazo)
-        print(resultado)
-        print(len(resultado))
-        sizeMensaje = len(mensaje)
-
-    dumpDataToMedia(rutaBase, data, password, resultado)
-
-def decode():
-    estimate = False
-    nameDeck = "PORRO"
-    media = getDeckMediaInformation(nameDeck, estimate)
-
-    if estimate:
-        manageEstimateMedia(media)
-    else:
-        password = "password"
-        data = utils.stringToHex("datos")
-        aw = AnkiWrapper.getInstance()
-        rutaBase = aw.rutaBase
-        updates = retrieveData(rutaBase, data, password, media)
-        print(updates)
-    return False
-
-def supossedMain():
-    estimate = False
-    nameDeck = "PORRO"
-    data = utils.stringToHex("datos")
-    password = "password"
-    print("Ocultando", data, "en mazo", nameDeck)
-    encodeDeck(nameDeck, data, password, estimate)
-    print("Leyendo data del mazo", nameDeck)
-    data = decodeDeck(nameDeck, password)
-    print("Data recuperada:", data)
-    print("Data recuperada:",utils.hexToString(data))
 
 def decodeDeck(nameDeck, password):
     media = getDeckMediaInformation(nameDeck, False)
@@ -113,9 +68,6 @@ def manageEstimateMedia(media):
 def getDeckMediaInformation(nameDeck, estimate=False):
     aw = AnkiWrapper.getInstance()
     deck = aw.getNotesFromDeck(nameDeck)
-    #print(aw.getDecks())
-    #print("---")
-    #print(deck)
     return buscarImagenesMazo(aw.rutaBase, deck, estimate)
 
 def readDataFromMedia(rutaBase, password, media):
@@ -170,50 +122,16 @@ def dumpDataToMedia(rutaBase, data, password, media):
 
     return pendingUpdates
 
-def retrieveData(rutaBase, data, password, media):
-    globalDataLength = len(data)
-    processedDataLength = 0
-    dataReader = DataBuffer(data)
-    end = False
-    pendingUpdates = []
-    for card in media:
-        for photo in card:
-            if photo["estimacion"] < 0:
-                print("Estimando: ", photo["name"])
-                photo["estimacion"] = stegoImage.estimate(rutaBase + photo["name"])
-            readData = dataReader.getNext(photo["estimacion"])
-            readDataLength = readData[1]
-            processedDataLength += readDataLength
-            readData = readData[0]
-            if readDataLength < photo["estimacion"]:
-                end = True
-            if readDataLength > 0:
-                #result = codificar(rutaBase, photo["index"], photo["name"], data, password)
-                result = decodificar(rutaBase, photo["name"], password)
-                print("Resultado: ",result)
-                if result:
-                    pendingUpdates.append(result)
-                else:
-                    print("photo problem detected: ", photo["name"], "index:", photo["index"])
-                    dataReader.goBack(readDataLength)
-                    end = False
-            if end:
-                break
-        if end:
-            break
-            
-    if processedDataLength < globalDataLength:
-        return False
-
-    return pendingUpdates
-
-    
-def buscarImagenesMazo(rutaBase, mazo, estimate=False):
-    lista = []
-    for index, row in mazo.iterrows():
-        resultadoAnalisis = analizarCard(rutaBase, row.name, row.flds, estimate)
-        if len(resultadoAnalisis) != 0:
-            lista.append(resultadoAnalisis)
-    return lista
+def main():
+    estimate = False
+    nameDeck = "PORRO"
+    data = utils.stringToHex("datos")
+    password = "password"
+    print("Ocultando", data, "en mazo", nameDeck)
+    encodeDeck(nameDeck, data, password, estimate)
+    print("Leyendo data del mazo", nameDeck)
+    data = decodeDeck(nameDeck, password)
+    print("Data recuperada:", data)
+    print("Data recuperada:",utils.hexToString(data))
 
 main()
